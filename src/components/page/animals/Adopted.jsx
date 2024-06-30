835e59d96c82dc714069181a5ac0e384a79ed5c7
Ivan GaLa <github@laodesign.es>
2024-06-30 23:14:43 +0200

    Modifico la página de adoptado.

--- src/components/page/animals/Adopted.jsx
+++ src/components/page/animals/Adopted.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Contexto from '../../contexts/Contexto'
 
 function Adopted() {
+
+  const { data } = useContext(Contexto)
+
   return (
     <div>
-      Adoptado!
+      <h2 className={data.estilos.h2}>Adoptado!</h2>
+      <p className={data.estilos.p}>Parece ser que el animal que estás buscando ya no está en nuestras instalación ¡Ha sido adoptado!</p>
     </div>
   )
 }
