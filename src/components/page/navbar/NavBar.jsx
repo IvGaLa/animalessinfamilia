4ff082554d410ec6b8bfb1ed5e6c8ac731c5f9d9
Ivan GaLa <github@laodesign.es>
2024-06-30 23:49:08 +0200

    Modifico el NavBar para que se parezca mas al footer.

--- src/components/page/navbar/NavBar.jsx
+++ src/components/page/navbar/NavBar.jsx
@@ -6,30 +6,32 @@ function NavBar() {
 
   // Creamos un array con los enlaces que queremos mostrar en el navbar
   const { data } = useContext(Contexto)
-  const enlaces = data.enlaces
-  const config = data.config
-  const linkstypes = data.linkstypes
 
   return (
-    <header className="text-gray-600 body-font">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <NavLink className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" to={'/'}>
-          <img src='/images/logo/64x64.png' alt={config.title} />
-          <span className="ml-3 text-xl">{config.title}</span>
-        </NavLink>
-        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-          {
-            enlaces.map((enlace, index) => (
-              (enlace.where.find(where => where === linkstypes.navbar)) ?
-                <NavLink key={index} className='mr-5 hover:text-gray-900' to={enlace.to}>{enlace.text}</NavLink>
-                : null
-            ))
-          }
-        </nav>
-      </div>
-    </header>
+    <div className='fixed top-0 w-full px-4'>
+      <header className="rounded-lg shadow bg-white">
+        <div className="p-1 md:flex md:items-center md:justify-between">
+          <span className="flex flex-row text-sm text-gray-800 sm:text-center items-center">
+            <img src='/images/logo/64x64.png' alt={data.config.title} />
+            <NavLink className="align-middle hover:underline" to={'/'}>
+              <span className="ml-3 text-xl">{data.config.title}</span>
+            </NavLink>
+          </span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-800 sm:mt-0">
+            {
+              data.enlaces.map((enlace, index) => (
+                (enlace.where.find(where => where === data.linkstypes.navbar)) ?
+                  <li key={index}>
+                    <NavLink className="hover:underline me-4 md:me-6" to={enlace.to}>{enlace.text}</NavLink>
+                  </li>
+                  : null
+              ))
+            }
+          </ul>
+        </div>
+      </header>
+    </div>
   )
-
 }
 
 export default NavBar
