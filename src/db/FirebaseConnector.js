9c26be27966f5c8b090ac6726095c5e06a989a82
Ivan GaLa <github@laodesign.es>
2024-07-09 11:29:11 +0200

    Iniciamos el uso de firebase/firestore

---
+++ src/db/FirebaseConnector.js
@@ -0,0 +1,7 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./firebaseConfig";
+
+
+const app = initializeApp(firebaseConfig);
+export const dbAnimales = getFirestore(app)
---
+++ src/db/firebaseConfig.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "YOUR_apiKey",
+  authDomain: "YOUR_authDomain",
+  projectId: "YOUR_projectId",
+  storageBucket: "YOUR_storageBucket",
+  messagingSenderId: "YOUR_messagingSenderId",
+  appId: "YOUR_appId",
+};
