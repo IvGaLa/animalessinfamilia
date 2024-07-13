a647ebfb4deb28ae23985a4f71f4609c391b7c89
Ivan GaLa <github@laodesign.es>
2024-07-13 23:11:00 +0200

    Modifico tamaño del spinner.

--- src/components/page/animals/AnimalGridLoading.jsx
+++ src/components/page/animals/AnimalGridLoading.jsx
@@ -6,7 +6,7 @@ function AnimalGridLoading() {
   const {data} = useContext(Contexto)
 
   return (
-    <img className="h-64 w-64" src={`${data.config.dirs.images}spinner.gif`} alt="Loading..." />
+    <img className="h-64 w-64" src={`${data.config.dirs.images}spinner_64x64.gif`} alt="Loading..." />
   )
 }
 
