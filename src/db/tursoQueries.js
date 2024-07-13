00612442bc0ec43fb2b7fe7cc15a7895c314d0e4
Ivan GaLa <github@laodesign.es>
2024-07-13 23:06:06 +0200

    Pequeñas correcciones.

--- src/db/tursoQueries.js
+++ src/db/tursoQueries.js
@@ -1,37 +1,32 @@
 import { turso } from './tursoClient';
 
 
-export const getAllAnimals = (setAnimales, data) => {
+export const getAllAnimals = (data) => {
   const table = data.config.turso.animalsTable
-  const perPage = data.config.turso.animalsPerPage
-  const sql = `SELECT * FROM ${table} ORDER BY name ASC LIMIT ${perPage};`
+  const sql = `SELECT * FROM ${table} ORDER BY name;`
 
   turso.execute(sql)
     .then((rs) => {
-      setAnimales(rs.rows)
+      return rs.rows
     })
 }
 
 
-export const getCount = (setCount = false, data) => {
+export const getCount = (data) => {
   const table = data.config.turso.animalsTable
   const sql = `select count(*) as totalAnimales from ${table};`
 
   turso.execute(sql)
     .then((rs) => {
-      if (setCount) {
-        setCount(rs.rows[0].totalAnimales)
-      } else {
-        return rs.rows[0].totalAnimales
-      }
+      return rs.rows[0].totalAnimales
     })
 }
 
 
-export const getNumPages = (setNumPages, data) => {
+export const getNumPages = (data) => {
   const perPage = data.config.turso.animalsPerPage
   const total = getCount(data)
   const totalPaginas = Math.ceil(total / perPage);
   const paginas = Array.from({ length: totalPaginas }, (v, i) => i + 1);
-  setNumPages(paginas)
+  return paginas
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css'
 import App from './App';
-import CtxProvider from './components/contexts/CtxProvider';
+import CtxProvider from 'components/contexts/CtxProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('animalessinfamilia'));
 root.render(
